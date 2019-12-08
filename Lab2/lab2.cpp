7fee10010b936fefc44406d2fdd766cc64ace019
Fadi Atamny <fadiatamny@gmail.com>
2019-12-08 22:13:57 +0200

    trying to figure out lab2 optional ex

--- Lab2/lab2.cpp
+++ Lab2/lab2.cpp
@@ -1,49 +1,77 @@
 #include <iostream>
+#include <fstream>
+#include <sstream>
 
 using namespace std;
 
-unsigned int seed = 0;
+ofstream out;
 
-void setseed(int s)
-{
-    seed = s;
-}
-
-unsigned int calc()
+unsigned int calc(unsigned int &seed)
 {
     seed = (seed * 1103515245) + 12345;
-    cout << seed << endl;
     return (seed / 65536) % 32768;
 }
 
-unsigned int test(unsigned int a,unsigned int b)
+void test1(unsigned int a, unsigned int b)
 {
-    a = a << 17;   
-    
+    out << "starting A is :" << a << " b is :" << b << endl
+        << endl
+        << endl;
+    a = a << 17;
+    int count = 0;
     unsigned int res = 0;
 
-    while(1)
+    out << "starting A after shift is :" << a << endl
+        << endl
+        << endl;
+
+    while (1)
     {
         res = (a / 65536) % 32768;
-        if(res == b)
-            return a;
+        if (res == b)
+        {
+            out << "answer is" << a << "res is something " << res << endl;
+            ++count;
+        }
+        if (count >= 1)
+            if (res != b)
+                break;
         a += 1;
     }
 }
 
-int main(int argc, char* argv[])
+void test2(unsigned int a, unsigned int b)
 {
-    unsigned int x = 0;
-    cin >> x;
+    unsigned int res = a * 65536;
 
-    setseed(x);
-    unsigned int a = calc();
-    cout << a << endl;
-    unsigned int b = calc();
-    cout << b << endl;
+    for (int i = 0; 32768; ++i, res++)
+    {
+        unsigned int tmp = res;
+        if (calc(tmp) == b)
+        {
+            out << res << endl;
+        }
+    }
+}
 
+int main(int argc, char *argv[])
+{
+    if (argc < 2)
+        return -1;
 
-    cout << test(a,b) << std::endl;
+    stringstream s(argv[1]);
+    out.open("log.txt");
 
+    unsigned int seed = 0;
+    s >> seed;
+
+    unsigned int a = calc(seed);
+    out << "seed divided to 15 bits is " << a << endl;
+    unsigned int b = calc(seed);
+    out << "seed divided to 15 bits is " << b << endl;
+
+    test1(a, b);
+    test2(a, b);
+    out.close();
     return 0;
 }
